6ff6177388d4ef394d1cf47efdac312eccdbaa70
ssabi <fhy1930@gmail.com>
2022-08-15 06:10:46 +0900

    기본 미들웨어 구현

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,37 @@
-require("dotenv").config();
-import express from "express";
+import * as dotenv from "dotenv";
+import express, { Request, Response, NextFunction } from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import path from "path";
 
+dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 3000;
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET || "some cookie",
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+    },
+    name: "session-cookie",
+  })
+);
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send(`<h1>Your Work Log</h1>`);
+});
 
 app.listen(port, () => {
-  console.info(`Server started at http://localhost:${port}`);
+  console.info(`Server listening on port ${port}`);
 });
